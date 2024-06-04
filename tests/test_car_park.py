a8682c50a3fd6fcfcd7ffbce1a8f1103e621b348
20055662 <20055662@tafe.wa.edu.au>
2024-06-04 16:58:52 +0800

    created test cases for sensor class

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -42,6 +42,12 @@ class TestCarPark(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.car_park.remove_car("NO-1")
 
+    def test_register_raises_type_error(self):
+        self.car_park = CarPark("123 Example Street", 100)
+        self.str = str()
+        with self.assertRaises(TypeError):
+            self.car_park.register("Not a Sensor or Display")
+
 
 if __name__ == "__main__":
     unittest.main()
